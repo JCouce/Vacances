9ae3277ce57eb790658205e8b089381ec2f8d0c7
Javier <jcoucelanga@gmail.com>
2019-07-17 13:10:35 +0200

    bad keys protection

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '572359933988-pqfl42hpsb6oeajljvo73ek9dolkurpr.apps.googleusercontent.com',
+    googleClientSecret: 'I-NZ0fBxu7b371nsLXqhIRDA',
+    mongoURI:'mongodb://javiercouce:bille123@ds351827.mlab.com:51827/vacaciones',
+    cookieKey: 'aiudapsodaosdoqjwoijqwoaidj'
+};
