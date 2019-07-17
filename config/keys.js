0f62026cca33d73fe8233f575eefabe683aa462c
Javier <jcoucelanga@gmail.com>
2019-07-17 13:47:14 +0200

    env var config

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '572359933988-pqfl42hpsb6oeajljvo73ek9dolkurpr.apps.googleusercontent.com',
-    googleClientSecret: 'I-NZ0fBxu7b371nsLXqhIRDA',
-    mongoURI:'mongodb://javiercouce:bille123@ds351827.mlab.com:51827/vacaciones',
-    cookieKey: 'aiudapsodaosdoqjwoijqwoaidj'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');   
+} else {
+    module.exports = require('./dev');   
+}
