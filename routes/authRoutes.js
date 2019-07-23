fe86a09127f2b765f0c5a05107c5c8fe769056cd
Javier <jcoucelanga@gmail.com>
2019-07-23 18:28:46 +0200

    removed / route in server

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,6 @@
 const passport = require ('passport');
 
 module.exports = app => {
-  app.get ('/', (req, res) => {
-    res.send (req.user);
-  });
   app.get ('/api/current_user', (req, res) => {
     res.send (req.user);
   });
